cca91640079be7455a08b72dab90b08fde11d2ec
kanakan12 <69573636+kanakan12@users.noreply.github.com>
2021-04-27 22:49:44 +0900

    Added components & routes

--- src/App.js
+++ src/App.js
@@ -1,28 +1,16 @@
 import React from "react";
-import PropTypes from "prop-types"
+import { HashRouter, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import About from "./routes/About";
+import Navigation from "./components/Navigation";
+import "./App.css";
 
-class App extends React.Component {
-  state = {
-    count: 0
-  };
-
-  add = () => {
-    this.setState(current => ({count: current.count + 1}));
-  };
-
-  minus = () => {
-    this.setState(current => ({count: current.count - 1}));
-  };
-
-  render(){
-    return (
-      <div>
-        <h1>The Number is {this.state.count}</h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
-      </div>
-    );
-  }
+function App() {
+  return <HashRouter>
+    <Navigation />
+    <Route path="/" exact={true} component={Home} />
+    <Route path="/about" component={About} />
+  </HashRouter>
 }
 
 export default App;
